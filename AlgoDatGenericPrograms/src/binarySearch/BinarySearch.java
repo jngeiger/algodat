14b740ee68c4ab12dfeed4d5ebb5d7352667b8be
Jan <Jan@JAN-PC.intern.geiger-digital.de>
2020-04-05 17:34:00 +0200

    test

---
+++ AlgoDatGenericPrograms/src/binarySearch/BinarySearch.java
@@ -0,0 +1,16 @@
+package binarySearch;
+
+public class BinarySearch {
+
+public static void main(String[] args)
+{
+	int[] array = new int[] {1,51,61,80,91,104,201,301,401,910,1001};
+	System.out.println(binarySearch(array,51));
+}
+
+public static int binarySearch(int[] array, int key)
+{
+	int i = array.length / 2;
+	while (array[i] != )
+}
+}
