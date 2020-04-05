88bde3d89dd3088be500e0a09607c0fc4c51ac74
Jan <Jan@JAN-PC.intern.geiger-digital.de>
2020-04-05 22:05:15 +0200

    cmt

--- AlgoDatGenericPrograms/src/binarySearch/BinarySearch.java
+++ AlgoDatGenericPrograms/src/binarySearch/BinarySearch.java
@@ -5,20 +5,21 @@ public class BinarySearch {
 public static void main(String[] args)
 {
 	int[] array = new int[] {1,51,61,80,91,104,201,301,401,910,1001};
-	System.out.println(binarySearch(array,910));
+	System.out.println(binarySearch(array,1001));
 }
 
 public static int binarySearch(int[] array, int key)
 {
-	int lo = 0; int hi = array.length;
-	while (lo <= hi)
+	int lo = 0; 
+	int high = array.length;
+	while (lo <= high)
 	{
-		int mid = lo + (hi - lo) / 2;
-		if (key < array[mid]) 
+		int mid = lo + (high - lo) / 2;
+		if (key < array[mid])
 		{
-			hi = mid - 1;
+			high = mid -1;
 		}
-		else if (key > array[mid])
+		if (key > array[mid])
 		{
 			lo = mid + 1;
 		}
