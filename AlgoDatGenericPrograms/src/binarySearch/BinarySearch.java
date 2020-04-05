a7bb3c1a4b40234b9e309d6b7ec87010056e143c
Jan <Jan@JAN-PC.intern.geiger-digital.de>
2020-04-05 19:01:16 +0200

    commit

--- AlgoDatGenericPrograms/src/binarySearch/BinarySearch.java
+++ AlgoDatGenericPrograms/src/binarySearch/BinarySearch.java
@@ -5,12 +5,28 @@ public class BinarySearch {
 public static void main(String[] args)
 {
 	int[] array = new int[] {1,51,61,80,91,104,201,301,401,910,1001};
-	System.out.println(binarySearch(array,51));
+	System.out.println(binarySearch(array,910));
 }
 
 public static int binarySearch(int[] array, int key)
 {
-	int i = array.length / 2;
-	while (array[i] != )
+	int lo = 0; int hi = array.length;
+	while (lo <= hi)
+	{
+		int mid = lo + (hi - lo) / 2;
+		if (key < array[mid]) 
+		{
+			hi = mid - 1;
+		}
+		else if (key > array[mid])
+		{
+			lo = mid + 1;
+		}
+		else
+		{
+			return mid;
+		}
+	}
+	return -1;
 }
 }
