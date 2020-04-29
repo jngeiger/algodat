10be10a263af1e645f5f026fd140d3861ce5b58d
Jan <Jan@JAN-PC.intern.geiger-digital.de>
2020-04-29 11:05:59 +0200

    push

--- AlgoDatGenericPrograms/src/list/List.java
+++ AlgoDatGenericPrograms/src/list/List.java
@@ -7,17 +7,78 @@ public class List<T> extends OurList<T> {
 	private Node head = null;
 	
 	public void insert(T ele) {
+		// list is empty
+		if (head == null)
+		{
+			head = new Node(ele);
+		}
+		else
+		{
+			Node temp = head;
+			head = new Node(ele);
+			head.next = temp;
+		}
 	}
 
 	public void insert(T ele, int pos) {
+		if (pos == 0 || head == null)
+		{
+			insert(ele);
+		}
+		else {
+			int idx = 1;
+			Node current = head;
+			while ((current.next != null) && (idx++ < pos))
+			{
+				current = current.next;
+			}
+			// Insert Node
+			Node temp = new Node(ele);
+			temp.next = current.next;
+			current.next = temp;
+		}
 	}
 
 	public boolean delete(Function<T, Boolean> func) {
+		Node current = head;
+		// Test whether first node has to be deleted
+		if (func.apply(current.data))
+		{
+			head = current.next;
+			return true;
+		}
+		
+		// Check rest of list
+		Node prev = current;
+		current = current.next;
+		while (current != null)
+		{
+			if (func.apply(current.data))
+			{
+				// node to be deleted has been found
+				prev.next = current.next;
+				return true;
+			}
+			// Update of current and previous
+			prev = prev.next;
+			current = current.next;
+		}
+		
 		return false;
 	}
 
 	public String toString() {
-		String retStr = "empty";
+		String retStr = "";
+		Node current = head;
+		while (current != null)
+		{
+			retStr += current.data;
+			current = current.next;
+			if (current != null)
+			{
+				retStr += " ";
+			}
+		}
 		return retStr;
 	}
 
@@ -40,12 +101,13 @@ public class List<T> extends OurList<T> {
 		l.insert(11);
 		l.insert(111);
 		l.insert(1111);
+		System.out.println(l + "\n");
 		l.insert(2,2);
 		
-		System.out.println(l);
+		System.out.println(l + "\n");
 		
-		Function<Integer, Boolean> isEven = a -> {return (a % 2) == 0;};
+		Function<Integer, Boolean> isEven = a -> {return (a % 2) != 0;};
 		l.delete(isEven);
-		System.out.println(l);
+		System.out.println(l + "\n");
 	}
 }
