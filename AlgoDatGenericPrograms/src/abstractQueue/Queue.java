812c60989aabcb03996615344621141aa7555992
jan <jan@jans-mbp.intern.geiger-digital.de>
2020-05-01 10:22:58 +0200

    push

--- AlgoDatGenericPrograms/src/abstractQueue/Queue.java
+++ AlgoDatGenericPrograms/src/abstractQueue/Queue.java
@@ -1,173 +1,245 @@
 package abstractQueue;
 
-import java.util.Arrays;
-
-public class Queue<T> extends SampleQueue<T> {
+public class Queue<T> extends SampleQueue<T> 
+{
+	
 private T[] array;
 private int first;
 private int last;
-
+private int count;
 
 @SuppressWarnings("unchecked")
 public Queue()
 {
-	array = (T[]) new Object[1];
-	first = 0;
-	last = 0;
+    this.first = 0;
+    this.last = 0;
+    array = (T[]) new Object[1];
+}
+   
+    protected T get() throws Exception
+    {
+    	if (isEmpty())
+    	{
+    		throw new Exception("Queue is empty");
+    	}
+    	
+    	if(first == array.length)
+    	{
+    		first = 0;
+    	}
+    	T temp;
+    	temp = array[first];
+    	array[first++] = null;
+    	count--;
+    	_shrinkIfNeeded();
+        return temp;
+    }
+
+
+    protected void put(T element) throws Exception 
+    {
+    	if(isFull())
+    	{
+    		_growIfNeeded();
+    		array[last++] = element;
+    		count++;
+    		return;
+    		
+    	}
+    	else if((last == array.length) && (array[0] == null))
+    	{
+    		last = 0;
+    		array[last++] = element;
+    		count++;
+    		return;
+    	}
+    	else
+    	{
+    		array[last++] = element;
+    		count++;
+    		return;
+    	}
+
+    }
+    
+    @SuppressWarnings("unchecked")
+    protected void _growIfNeeded() throws Exception
+    {
+    	T[] newArr = (T[])new Object[array.length*2];
+    	for(int i=0; i<array.length; i++)
+    	{
+    		newArr[i] = get();
+    		count++;
+    	}
+    	array = newArr;
+    	first = 0;
+    	last = (array.length/2);
+    }
+    
+    private void _shrinkIfNeeded() throws Exception
+    {
+        if (count < (array.length / 4))
+        {
+        	int temp = count;
+            T[] newArr = (T[]) new Object[array.length/2];
+            for(int i=0; i<temp; i++)
+            {
+            	newArr[i] = get();
+            	count++;
+            }
+            array = newArr;
+            first = 0;
+            last = (count);
+        }
+    }
+    
+    protected boolean isFull()
+    {
+    	return (count == array.length);
+    }
+
+
+    protected boolean isEmpty()
+    {
+    	return (count == 0);
+    }
+    
+    public int getArraySize()
+    {
+    return array.length;
+    }
+    
+    public static void main(String [] args) throws Exception
+    {
+//    	Queue<String> q = new Queue<String>();
+//        
+//    	
+//    	q.put("Hello");
+//    	q.put("World");
+//    	q.put("Test");
+// 
+//    	
+//    	System.out.println(q.get());
+//    	System.out.println(q.get());
+//    	System.out.println(q.get());
+//    
+//    	
+//    	q.put("test2");
+//    	q.put("asdfasdf");
+//    	q.put("blbalaldsfk");
+//    	System.out.println(q.get());
+//    	
+//    	q.put("asdfasdfasdfgysvsdfjasdufjsadfj");
+//    	
+//    	System.out.println(q.get());
+//    	System.out.println(q.get());
+//    	
+//    	q.put("blablatest");
+//    	
+//    	
+//    	Queue<String> q2 = new Queue<String>();
+//    	
+//    	q2.put("Hello");
+//    	q2.put("World");
+//    	q2.put("Servus");
+//    	q2.put("Welt");
+//    	
+//    	
+//    	
+//    	System.out.println(q2.get());
+//    	System.out.println(q2.get());
+//    	q2.put("Hello");
+//    	q2.put("World");
+//    	System.out.println(q2.get());
+//    	System.out.println(q2.get());
+//    	System.out.println(q2.get());
+//    	System.out.println(q2.get());
+    	
+    	Queue<Integer> q=new Queue<Integer>();
+
+		System.out.println("");
+		System.out.println("===TEST0===");
+		System.out.println(q.isEmpty());
+		System.out.println(q.get());
+		System.out.println(q.isEmpty());
+		System.out.println(q.get());
+		System.out.println(q.get());
+		System.out.println(q.isEmpty());
+
+		System.out.println("");
+		System.out.println("===TEST1===");
+		System.out.println(q.isEmpty());
+		q.put(11);
+		System.out.println(q.isEmpty());			
+		System.out.println(q.get());
+		System.out.println(q.isEmpty());
+
+		System.out.println("");
+		System.out.println("===TEST2===");
+		System.out.println(q.getArraySize());
+		q.put(21);
+		System.out.println(q.get());
+		System.out.println(q.get());
+
+		System.out.println("");
+		System.out.println("===TEST3===");
+		q.put(31);
+		q.put(32);
+		q.put(33);
+		q.put(34);
+		q.put(35);
+		q.put(36);
+		q.put(37);
+		q.put(38);
+		System.out.println(q.get());
+		System.out.println(q.get());
+		System.out.println(q.get());
+		System.out.println(q.get());
+		System.out.println(q.get());
+		System.out.println(q.get());
+		System.out.println(q.get());
+		System.out.println(q.get());
+		System.out.println(q.get());
+		System.out.println(q.get());
+		System.out.println(q.get());
+		System.out.println(q.get());
+		System.out.println(q.get());
+
+		System.out.println("");
+		System.out.println("===TEST4===");
+		q.put(41);
+		q.put(42);
+		q.put(43);
+		q.put(44);
+		q.put(45);
+		q.put(46);
+		q.put(47);
+		q.put(48);
+
+
+		q.put(49);
+		q.put(410);
+		q.put(411);
+		System.out.println(q.get());
+		System.out.println(q.get());
+		System.out.println(q.get());
+		System.out.println(q.get());
+		System.out.println(q.get());
+		System.out.println(q.get());
+		System.out.println(q.get());
+		System.out.println(q.get());
+		System.out.println(q.get());
+		System.out.println(q.get());
+		System.out.println(q.get());
+		System.out.println(q.get());
+		System.out.println(q.get());
+    	
+    	
+  
+   
+    }
+
 }
 
-public boolean isEmpty()
-{
-	return (((first == last) && first == array.length) || ((first == last) && array[first] == null));
-}
 
-public T get() throws Exception
-{
-	if (isEmpty())
-	{
-		throw new Exception("Queue empty (get)");
-	}
-	if (first == array.length)
-	{
-		first = 0;
-	}
-	
-	
-	T temp = array[first];
-	array[first] = null;
-	first++;
-	
-	//_shrinkIfNeeded(array.length / 2);
-	
-	return temp;
-	
-}
 
-public void put(T element) throws Exception
-{
-	_growIfNeeded(2*array.length);
-	
-	if ((last == array.length) && (array[0] == null))
-	{
-		last = 0;
-	}
-	
-	array[last++] = element;
-	
-}
-
-@SuppressWarnings("unchecked")
-private void _growIfNeeded(int size) throws Exception 
-{
-	if (_getArrayElements() == array.length)
-	{
-	T[] temp = (T[]) new Object[size];
-	int i = 0;
-	
-	while (!isEmpty())
-	{
-		temp[i] = get();
-		i++;
-	}
-	first = 0;
-	last = i;
-	array = temp;
-	}
-}
-	
-@SuppressWarnings("unchecked")
-private void _shrinkIfNeeded(int size)
-{
-	if (_getArrayElements() <= array.length / 4)
-	{
-		T[] temp = (T[]) new Object[size];
-		int counter = 0;
-		for (int i = 0; i < array.length; i++)
-		{
-			if (array[i] != null)
-			temp[counter++] = array[i];
-		}
-		first = 0;
-		last = temp.length - 1;
-		array = temp;
-	}
-}
-
-private int _getArrayElements()
-{
-	int count = 0;
-	for (int i = 0; i < array.length; i++)
-	{
-		if (array[i] != null)
-		{
-			count++;
-		}
-	}
-	return count;
-}
-
-private void _debug()
-{
-	System.out.println("First: " + this.first + " Last: " + this.last + " Array Size: " + this.array.length);
-}
-
-	public static void main(String[] args) throws Exception {
-		// TODO Auto-generated method stub
-		Queue<String> q1 = new Queue<String>();
-		
-		q1.put("Hello");
-		q1._debug(); //First: 0 Last: 1 Array Size: 1
-		System.out.println(Arrays.deepToString(q1.array));
-	
-		
-		q1.put("World"); 
-		System.out.println(Arrays.deepToString(q1.array));
-		q1._debug(); //First: 0 Last: 2 Array Size: 2
-		
-		q1.put("Test");
-		System.out.println(Arrays.deepToString(q1.array));
-		q1._debug();
-		q1.put("123");
-		q1._debug(); // First: 0 Last: 4 Array Size: 4
-		System.out.println(q1.get());  // Hello
-		q1._debug();
-		q1.put("another test");
-		q1._debug(); //First: 1 Last: 1 Array Size: 4
-		System.out.println(Arrays.deepToString(q1.array));
-		
-		System.out.println(q1.get()); // World
-		q1.put("Blub");
-		System.out.println(q1.get()); // Test
-		System.out.println(q1.get()); // 123
-		q1._debug(); // First: 4 Last: 2 Array Size: 4
-		System.out.println(q1._getArrayElements()); // 2 -> OK
-		System.out.println(q1.get());// another test
-		System.out.println(q1._getArrayElements()); // 1 - > OK
-		q1._debug(); // First: 0 Last: 1 Array Size: 2 -> OK
-		System.out.println(q1.get()); //-> Blub
-		q1._debug(); // Array Size: 1 -> OK
-		System.out.println(q1._getArrayElements()); // 0 -> OK
-		q1.put("c");
-		q1.put("s");
-		q1.put("t");
-		q1.put("j");
-		System.out.println(q1._getArrayElements());
-		System.out.println(q1.get());
-		System.out.println(q1.get());
-		q1.put("f");
-		q1.put("t");
-		q1.put("jk");
-		q1._debug();
-		
-		System.out.println(q1.get());
-		System.out.println(q1.get());
-		System.out.println(q1.get());
-		System.out.println(q1.get());
-		System.out.println(q1.get());
-//		
-
-		
-		
-	}
-
-}
