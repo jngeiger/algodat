88acf9a531ef9af4b98980b062b0da1af9d781dd
jan <jan@jans-mbp.intern.geiger-digital.de>
2020-05-05 00:36:57 +0200

    not yet optimized

---
+++ AlgoDatGenericPrograms/src/recursiveBinarySearch/RecursiveBinarySearch.java
@@ -0,0 +1,80 @@
+package recursiveBinarySearch;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RecursiveBinarySearch<T extends Comparable<T>> {
+private List<T> sortedList = null;
+private int counter;
+	
+public RecursiveBinarySearch(List<T> list)
+{
+	list.sort((x,y) -> x.compareTo(y));
+	sortedList = list;
+}
+
+public T search(T value) throws Exception
+{
+	return recursion(value, sortedList);
+}
+
+public T recursion(T value, List<T> list) throws Exception
+{
+	counter++;
+	if (list.size() == 1)
+	{
+		if (value.compareTo(list.get(0)) == 0)
+		{
+			return (list.get(0));
+		}
+		else
+		{
+			throw new Exception("Not in List");
+		}
+	}
+	if (value.compareTo(list.get(list.size() / 2 - 1)) < 0)
+	{
+		return recursion(value,list.subList(0, (list.size()/2)));
+	}
+	else
+	{
+		return recursion(value,list.subList(list.size() / 2 , (list.size())));
+	}
+}
+
+
+
+public String toString()
+{
+	return sortedList.toString();
+}
+
+
+public static void main(String[] args) throws Exception
+{
+	var list = new ArrayList<Integer>();
+//	for(int i = 0; i < 2000; i++)
+//	{
+//		list.add(Integer.valueOf((int) (Math.random() * 100)+1));
+//	}
+	list.add(1);
+	list.add(2);
+	list.add(3);
+	list.add(4);
+	list.add(5);
+	list.add(42);	
+	list.add(6);
+	list.add(41);
+	list.add(38);
+	list.add(22);
+	list.add(7);
+	list.add(8);
+	
+	RecursiveBinarySearch<Integer> search = new RecursiveBinarySearch<Integer>(list);
+	System.out.println(list);
+	search.search(42);
+
+	System.out.println(search.counter);
+	
+}
+}
