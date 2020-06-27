62b1fb1e6ad94bb49e102041278d68a8bfb94268
Jan-Niklas Geiger <jan@jans-mbp.intern.geiger-digital.de>
2020-06-27 15:37:59 +0200

    push

--- AlgoDatGenericPrograms/src/avl/First.java
+++ AlgoDatGenericPrograms/src/avl/First.java
@@ -4,12 +4,12 @@ public class First {
     
     public static void main(String args[])
     {        
-        int complexContent[] = {10,20,15,25,30};
+        int complexContent[] = {10,5,18,4,16,19,17};
         AVLTree<Integer> complexTree = new AVLTree<Integer>();
         for (int v : complexContent)
             complexTree.insertRec(v);
         System.out.println(complexTree);
-        complexTree.insertRec(22);
+       complexTree.delete(10);
         System.out.println(complexTree);
     }
 }
