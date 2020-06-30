ae14fee986e40ef576456a65abb71a0ef3b22f4b
Jan <Jan@JAN-PC.intern.geiger-digital.de>
2020-06-30 20:32:54 +0200

    push

--- AlgoDatGenericPrograms/src/splay/First.java
+++ AlgoDatGenericPrograms/src/splay/First.java
@@ -5,31 +5,22 @@ public class First {
     public static void main(String args[])
     {
         SplayTree<Integer> complexTree = new SplayTree<Integer>();
+        int[] array = { 9,3,8,2,9,14,7,18,15,15,20};
+//        int [] array = {7,18,15,15,20};
+       	for (int i = 0; i < 11; i++)
+       	{
+       		complexTree.insert(array[i]);
+       	}
+        
+//       complexTree.insert(14);
+        complexTree.splay(7);
+        complexTree.splay(18);
+        complexTree.splay(15);
+        complexTree.splay(15);
+        complexTree.splay(20);
+        System.out.println(complexTree);
 
-        complexTree.insert(2);
-        complexTree.insert(126);
-        complexTree.insert(4);
-        complexTree.insert(3);
-        complexTree.insert(12);
-        complexTree.insert(13);
-        complexTree.insert(15);
-        complexTree.insert(28);
-        complexTree.insert(29);
-        complexTree.insert(315);
-        complexTree.insert(32);
-        complexTree.insert(57);
-        complexTree.insert(60);
-        complexTree.insert(99);
-        complexTree.insert(77);
-        complexTree.insert(21);
-        complexTree.insert(18);
-        
-        
-//        complexTree.splay(67);
-        System.out.println(complexTree);
- complexTree.splay_delete(12);
-        
-        System.out.println(complexTree);
+      
        
         
 
