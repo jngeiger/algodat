2fabf9d4d0e1cd55f380a1de9549bd253ce03117
Jan <Jan@JAN-PC.intern.geiger-digital.de>
2020-07-05 17:33:41 +0200

    push

--- AlgoDatGenericPrograms/src/splay/First.java
+++ AlgoDatGenericPrograms/src/splay/First.java
@@ -1,20 +1,21 @@
 package splay;
 
+import java.util.Arrays;
+
 public class First {
     
     public static void main(String args[])
     {
         SplayTree<Integer> complexTree = new SplayTree<Integer>();
         SplayTree<Character> sptree = new SplayTree<Character>();
-        Character[] starray = {'A','T','T','R','I','B','U','T'};
+        Character[] starray = {'P','O','R','S','C','H','E'};
         
         int [] array = {0,19,19,17,8,1,20,19};
-       	for (int i = 0; i < array.length; i++)
+       	for (int i = 0; i < starray.length; i++)
        	{
        		sptree.insert(starray[i]);
        	}
-        
-       	
+       	sptree.splay_delete('S');
 //       complexTree.insert(14);
        
         System.out.println(sptree);
